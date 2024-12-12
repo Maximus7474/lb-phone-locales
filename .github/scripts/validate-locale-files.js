2bd22758688a29472ece20364682c0c1e0f1a8e4
Maximus7474 <the.maximus.7474@gmail.com>
2024-12-13 00:30:59 +0100

    test changes for ci

--- .github/scripts/validate-locale-files.js
+++ .github/scripts/validate-locale-files.js
@@ -68,7 +68,7 @@ function validateJsonFile(fileName, skipRecap) {
         return true;
     } catch (error) {
         console.error(`File '${fileName}' did not pass the checks`);
-        process.exitCode = 1;
+        if (!skipRecap) process.exitCode = 1;
         return false;
     }
 }
